9ea72849cb169c3b64686647eed6202b326a40dd
Tiago Gonçalves <tiagogn@gmail.com>
2025-02-16 16:17:02 -0300

    feat: retry para pedido feat: ssl para documentDb

---
+++ src/main/kotlin/br/com/fiap/lanchonete/pagamento/adapters/config/ConfigDocumentDB.kt
@@ -0,0 +1,34 @@
+package br.com.fiap.lanchonete.pagamento.adapters.config
+
+import com.mongodb.MongoClientSettings
+import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer
+import org.springframework.boot.autoconfigure.mongo.MongoProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ConfigDocumentDB(
+
+) {
+
+    @Bean
+    fun mongoClientSettings(): MongoClientSettings {
+        var JAVA_HOME = System.getenv("JAVA_HOME")
+        System.setProperty("javax.net.ssl.trustStore", "$JAVA_HOME/lib/security/cacerts")
+        System.setProperty("javax.net.ssl.trustStorePassword", "changeit")
+        return MongoClientSettings.builder()
+            .applyToSslSettings{ builder ->
+                builder.enabled(true)
+            }
+            .build()
+    }
+
+    @Bean
+    fun mongoClientSettingsBuilderCustomizer(): MongoClientSettingsBuilderCustomizer {
+        return MongoClientSettingsBuilderCustomizer { builder ->
+            builder.applyToSslSettings { ssl ->
+                ssl.enabled(true)
+            }
+        }
+    }
+}
--- src/main/kotlin/br/com/fiap/lanchonete/pagamento/adapters/output/gateway/PedidoGatewayAdapter.kt
+++ src/main/kotlin/br/com/fiap/lanchonete/pagamento/adapters/output/gateway/PedidoGatewayAdapter.kt
@@ -4,16 +4,23 @@ import br.com.fiap.lanchonete.pagamento.adapters.output.client.PedidoClient
 import br.com.fiap.lanchonete.pagamento.core.application.ports.output.gateway.PedidoGateway
 import br.com.fiap.lanchonete.pagamento.core.dto.PedidoInput
 import br.com.fiap.lanchonete.pagamento.core.dto.PedidoOutput
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.EnableRetry
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
 
 @Component
+@EnableRetry
 class PedidoGatewayAdapter(
     private val pedidoClient: PedidoClient
 ): PedidoGateway {
+
+    @Retryable(maxAttempts = 3, backoff = Backoff(delay = 1000, multiplier = 1.5))
     override fun consultarPedido(pedidoId: String): PedidoInput {
         return pedidoClient.consultarPedido(pedidoId)
     }
 
+    @Retryable(maxAttempts = 3, backoff = Backoff(delay = 1000, multiplier = 1.5))
     override fun confirmarPagamento(pedidoId: String, pedidoOutput: PedidoOutput) {
         pedidoClient.confirmarPagamento(pedidoId, pedidoOutput)
     }
